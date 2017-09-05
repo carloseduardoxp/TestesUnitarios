5cfd4b4dac2d9c58fe2db8cb2d1d9ba2d2aeea69
carlo <carlo@DESKTOP-T5V92F4>
2017-09-04 22:00:38 -0300

    Versão inicial

---
+++ src/Lance.java
@@ -0,0 +1,21 @@
+
+public class Lance {
+	
+	private Usuario usuario;
+	
+	private Double valor;
+
+	public Lance(Usuario usuario, Double valor) {
+		this.usuario = usuario;
+		this.valor = valor;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+}
---
+++ src/Usuario.java
@@ -0,0 +1,14 @@
+
+public class Usuario {
+	
+	private String login; 
+
+	public Usuario(String login) {
+		this.login = login;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+}
